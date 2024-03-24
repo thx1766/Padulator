39e1e23ee948aaa6c7a21bb1575c61f3e6db541f
Nate Schaffner <thx1766@gmail.com>
2024-03-24 16:04:34 -0400

    Initial Commit

---
+++ Padulator/PadulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadulatorApp.swift
+//  Padulator
+//
+//  Created by Nate Schaffner on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PadulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
